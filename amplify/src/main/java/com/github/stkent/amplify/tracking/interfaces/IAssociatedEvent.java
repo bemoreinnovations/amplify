ca119b267629f4ed5ea969001edfe5d200230bb4
mindgrub-cdillon <cdillon@mindgrub.com>
2016-11-14 23:19:01 -0500

    adding associated event interface

--- amplify/src/main/java/com/github/stkent/amplify/tracking/interfaces/IAssociatedEvent.java
+++ amplify/src/main/java/com/github/stkent/amplify/tracking/interfaces/IAssociatedEvent.java
@@ -3,15 +3,8 @@ package com.github.stkent.amplify.tracking.interfaces;
 /**
  * Created by Cody on 11/14/16.
  */
-public abstract class IAssociatedEvent implements IEvent {
+public interface IAssociatedEvent extends IEvent {
 
-    private IEvent associatedEvent;
-
-    public IEvent getAssociatedEvent() {
-        return associatedEvent;
-    }
-
-    public void setAssociatedEvent(IEvent associatedEvent) {
-        this.associatedEvent = associatedEvent;
-    }
+    IEvent getAssociatedEvent();
+    void setAssociatedEvent(IEvent event);
 }
